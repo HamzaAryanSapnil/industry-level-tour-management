b2e5b9a4b8b7b9238da8c2632d0871504d206da3
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2025-07-09 13:18:50 +0600

    setup completed

---
+++ src/app/config/env.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+interface EnvConfig {
+  PORT: string;
+  MONGODB_URI:string;
+  NODE_ENV: "development" | "production" ;
+}
+
+const loadEnvVariables = (): EnvConfig => {
+
+  const requiredEnvVariables: string[] = ['PORT', 'MONGODB_URI', 'NODE_ENV', ]
+  requiredEnvVariables.forEach(variable => {
+    if (!process.env[variable]) {
+      throw new Error(`Missing env variable ${variable}`)
+    }
+  })
+
+  return {
+    PORT: process.env.PORT as string,
+    MONGODB_URI: process.env.MONGODB_URI as string,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
+  }
+}
+
+export const envVars: EnvConfig = loadEnvVariables()
---
+++ src/server.ts
@@ -0,0 +1,65 @@
+/* eslint-disable no-console */
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./app/config/env";
+
+let server: Server;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(envVars.MONGODB_URI);
+    console.log("connected to db");
+
+    server = app.listen(envVars.PORT, async () => {
+      console.log(`server is listening on port ${envVars.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+// *unhandled
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected.. Server is shutting down", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+// *uncaugth
+process.on("uncaughtException", (err) => {
+  console.log("Uncaugth Exception detected.. Server is shutting down", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("SIGTERM", () => {
+  console.log("Sigterm signal received.. Server is shutting down");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+process.on("SIGINT", () => {
+  console.log("Sigint signal received.. Server is shutting down");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
