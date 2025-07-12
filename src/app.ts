a835be0e9180864e887c44a30d2dff7c1d653a37
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2025-07-12 16:07:38 +0600

    added utils functions in part 1

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
+
 import express, { Request, Response } from "express";
+import cors from "cors";
+import { router } from "./app/routes";
+import { globalErrorHandlers } from "./app/middlewares/global.error.handlers";
+import notFound from './app/middlewares/notFound';
+
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1", router);
 
 app.get("/", async (req: Request, res: Response) => {
   res.status(200).send({
@@ -7,5 +17,8 @@ app.get("/", async (req: Request, res: Response) => {
     message: "Welcome to tour management system",
   });
 });
+
+app.use(globalErrorHandlers);
+app.use(notFound)
 
 export default app;
---
+++ src/app/modules/user/user.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { UserControllers } from "./user.controller";
+
+const router = Router();
+
+router.get("/all-users", UserControllers.getAllUsers);
+router.post("/register", UserControllers.createUser);
+
+export const UserRoutes = router;
